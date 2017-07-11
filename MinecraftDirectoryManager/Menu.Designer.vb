44a7b203fd9ee4fc77d054194a651790a3fd3674
QuasarX1 - BuildTools <chris.rowe19@outlook.com>
2017-07-11 22:58:01 +0100

    Directories tab interface in main menu set up

--- MinecraftDirectoryManager/Menu.Designer.vb
+++ MinecraftDirectoryManager/Menu.Designer.vb
@@ -24,11 +24,18 @@ Partial Class Menu
     Private Sub InitializeComponent()
         Me.MainMenu_TabControl = New System.Windows.Forms.TabControl()
         Me.Directories_TabPage = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Mods_TabPage = New System.Windows.Forms.TabPage()
         Me.DirectoryNames_ListBox = New System.Windows.Forms.ListBox()
         Me.AddDirectory_Button = New System.Windows.Forms.Button()
         Me.DeleteDirectory_Button = New System.Windows.Forms.Button()
-        Me.EditDirectory_Button = New System.Windows.Forms.Button()
+        Me.UpdateDirectoryInfo_Button = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.DirectoryName_Label = New System.Windows.Forms.Label()
+        Me.DirectoryPath_Label = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.NavToDirectory_Button = New System.Windows.Forms.Button()
+        Me.DirectoryInfo_Label = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
         Me.MainMenu_TabControl.SuspendLayout()
         Me.Directories_TabPage.SuspendLayout()
         Me.SuspendLayout()
@@ -36,7 +43,7 @@ Partial Class Menu
         'MainMenu_TabControl
         '
         Me.MainMenu_TabControl.Controls.Add(Me.Directories_TabPage)
-        Me.MainMenu_TabControl.Controls.Add(Me.TabPage2)
+        Me.MainMenu_TabControl.Controls.Add(Me.Mods_TabPage)
         Me.MainMenu_TabControl.Location = New System.Drawing.Point(12, 12)
         Me.MainMenu_TabControl.Name = "MainMenu_TabControl"
         Me.MainMenu_TabControl.SelectedIndex = 0
@@ -45,7 +52,14 @@ Partial Class Menu
         '
         'Directories_TabPage
         '
-        Me.Directories_TabPage.Controls.Add(Me.EditDirectory_Button)
+        Me.Directories_TabPage.Controls.Add(Me.RichTextBox1)
+        Me.Directories_TabPage.Controls.Add(Me.DirectoryInfo_Label)
+        Me.Directories_TabPage.Controls.Add(Me.NavToDirectory_Button)
+        Me.Directories_TabPage.Controls.Add(Me.TextBox2)
+        Me.Directories_TabPage.Controls.Add(Me.DirectoryPath_Label)
+        Me.Directories_TabPage.Controls.Add(Me.DirectoryName_Label)
+        Me.Directories_TabPage.Controls.Add(Me.TextBox1)
+        Me.Directories_TabPage.Controls.Add(Me.UpdateDirectoryInfo_Button)
         Me.Directories_TabPage.Controls.Add(Me.DeleteDirectory_Button)
         Me.Directories_TabPage.Controls.Add(Me.AddDirectory_Button)
         Me.Directories_TabPage.Controls.Add(Me.DirectoryNames_ListBox)
@@ -57,15 +71,15 @@ Partial Class Menu
         Me.Directories_TabPage.Text = "Directories"
         Me.Directories_TabPage.UseVisualStyleBackColor = True
         '
-        'TabPage2
+        'Mods_TabPage
         '
-        Me.TabPage2.Location = New System.Drawing.Point(8, 39)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(1258, 733)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "TabPage2"
-        Me.TabPage2.UseVisualStyleBackColor = True
+        Me.Mods_TabPage.Location = New System.Drawing.Point(8, 39)
+        Me.Mods_TabPage.Name = "Mods_TabPage"
+        Me.Mods_TabPage.Padding = New System.Windows.Forms.Padding(3)
+        Me.Mods_TabPage.Size = New System.Drawing.Size(1258, 733)
+        Me.Mods_TabPage.TabIndex = 1
+        Me.Mods_TabPage.Text = "Mods"
+        Me.Mods_TabPage.UseVisualStyleBackColor = True
         '
         'DirectoryNames_ListBox
         '
@@ -73,7 +87,7 @@ Partial Class Menu
         Me.DirectoryNames_ListBox.ItemHeight = 25
         Me.DirectoryNames_ListBox.Location = New System.Drawing.Point(237, 31)
         Me.DirectoryNames_ListBox.Name = "DirectoryNames_ListBox"
-        Me.DirectoryNames_ListBox.Size = New System.Drawing.Size(233, 654)
+        Me.DirectoryNames_ListBox.Size = New System.Drawing.Size(233, 354)
         Me.DirectoryNames_ListBox.TabIndex = 0
         '
         'AddDirectory_Button
@@ -87,21 +101,79 @@ Partial Class Menu
         '
         'DeleteDirectory_Button
         '
-        Me.DeleteDirectory_Button.Location = New System.Drawing.Point(27, 285)
+        Me.DeleteDirectory_Button.Location = New System.Drawing.Point(27, 294)
         Me.DeleteDirectory_Button.Name = "DeleteDirectory_Button"
         Me.DeleteDirectory_Button.Size = New System.Drawing.Size(178, 91)
         Me.DeleteDirectory_Button.TabIndex = 2
         Me.DeleteDirectory_Button.Text = "Delete Directory"
         Me.DeleteDirectory_Button.UseVisualStyleBackColor = True
         '
-        'EditDirectory_Button
+        'UpdateDirectoryInfo_Button
         '
-        Me.EditDirectory_Button.Location = New System.Drawing.Point(27, 157)
-        Me.EditDirectory_Button.Name = "EditDirectory_Button"
-        Me.EditDirectory_Button.Size = New System.Drawing.Size(178, 91)
-        Me.EditDirectory_Button.TabIndex = 3
-        Me.EditDirectory_Button.Text = "Edit Directory"
-        Me.EditDirectory_Button.UseVisualStyleBackColor = True
+        Me.UpdateDirectoryInfo_Button.Location = New System.Drawing.Point(696, 361)
+        Me.UpdateDirectoryInfo_Button.Name = "UpdateDirectoryInfo_Button"
+        Me.UpdateDirectoryInfo_Button.Size = New System.Drawing.Size(178, 91)
+        Me.UpdateDirectoryInfo_Button.TabIndex = 3
+        Me.UpdateDirectoryInfo_Button.Text = "Update Directory Info"
+        Me.UpdateDirectoryInfo_Button.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(696, 31)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(263, 31)
+        Me.TextBox1.TabIndex = 4
+        '
+        'DirectoryName_Label
+        '
+        Me.DirectoryName_Label.AutoSize = True
+        Me.DirectoryName_Label.Location = New System.Drawing.Point(504, 37)
+        Me.DirectoryName_Label.Name = "DirectoryName_Label"
+        Me.DirectoryName_Label.Size = New System.Drawing.Size(160, 25)
+        Me.DirectoryName_Label.TabIndex = 5
+        Me.DirectoryName_Label.Text = "Directory Name"
+        '
+        'DirectoryPath_Label
+        '
+        Me.DirectoryPath_Label.AutoSize = True
+        Me.DirectoryPath_Label.Location = New System.Drawing.Point(516, 97)
+        Me.DirectoryPath_Label.Name = "DirectoryPath_Label"
+        Me.DirectoryPath_Label.Size = New System.Drawing.Size(148, 25)
+        Me.DirectoryPath_Label.TabIndex = 6
+        Me.DirectoryPath_Label.Text = "Directory Path"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(696, 97)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(263, 31)
+        Me.TextBox2.TabIndex = 7
+        '
+        'NavToDirectory_Button
+        '
+        Me.NavToDirectory_Button.Location = New System.Drawing.Point(1002, 97)
+        Me.NavToDirectory_Button.Name = "NavToDirectory_Button"
+        Me.NavToDirectory_Button.Size = New System.Drawing.Size(153, 30)
+        Me.NavToDirectory_Button.TabIndex = 8
+        Me.NavToDirectory_Button.Text = "Navigate To"
+        Me.NavToDirectory_Button.UseVisualStyleBackColor = True
+        '
+        'DirectoryInfo_Label
+        '
+        Me.DirectoryInfo_Label.AutoSize = True
+        Me.DirectoryInfo_Label.Location = New System.Drawing.Point(525, 157)
+        Me.DirectoryInfo_Label.Name = "DirectoryInfo_Label"
+        Me.DirectoryInfo_Label.Size = New System.Drawing.Size(139, 25)
+        Me.DirectoryInfo_Label.TabIndex = 9
+        Me.DirectoryInfo_Label.Text = "Directory Info"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(696, 157)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(263, 162)
+        Me.RichTextBox1.TabIndex = 10
+        Me.RichTextBox1.Text = ""
         '
         'Menu
         '
@@ -110,18 +182,26 @@ Partial Class Menu
         Me.ClientSize = New System.Drawing.Size(1298, 804)
         Me.Controls.Add(Me.MainMenu_TabControl)
         Me.Name = "Menu"
-        Me.Text = "Form1"
+        Me.Text = "Main Menu"
         Me.MainMenu_TabControl.ResumeLayout(False)
         Me.Directories_TabPage.ResumeLayout(False)
+        Me.Directories_TabPage.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents MainMenu_TabControl As TabControl
     Friend WithEvents Directories_TabPage As TabPage
-    Friend WithEvents EditDirectory_Button As Button
+    Friend WithEvents UpdateDirectoryInfo_Button As Button
     Friend WithEvents DeleteDirectory_Button As Button
     Friend WithEvents AddDirectory_Button As Button
     Friend WithEvents DirectoryNames_ListBox As ListBox
-    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents Mods_TabPage As TabPage
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents DirectoryInfo_Label As Label
+    Friend WithEvents NavToDirectory_Button As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents DirectoryPath_Label As Label
+    Friend WithEvents DirectoryName_Label As Label
+    Friend WithEvents TextBox1 As TextBox
 End Class
