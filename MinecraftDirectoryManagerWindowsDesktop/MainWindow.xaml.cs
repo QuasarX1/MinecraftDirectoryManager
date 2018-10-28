ba209f9e54560cb99456471e9d22e68d1bf6b654
Christopher Rowe <chris.rowe19@outlook.com>
2018-10-28 11:06:54 +0000

    Added a new project for the windows desktop and added some basic navigation functionality with some pages.

---
+++ MinecraftDirectoryManagerWindowsDesktop/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MinecraftDirectoryManagerWindowsDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private object homePage = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MainDisplayFrame.Content = new HomePage();
+        }
+
+        public void NavigateToPage(Type pageType)
+        {
+            MainDisplayFrame.Content = Activator.CreateInstance(pageType);
+        }
+
+
+
+        private void HomeMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToPage(typeof(HomePage));
+        }
+
+        private void DirectoriesMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToPage(typeof(DirectoriesPage));
+        }
+
+        private void SavesMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToPage(typeof(SavesPage));
+        }
+
+        private void VersionsMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToPage(typeof(VersionsPage));
+        }
+
+        private void ModsMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToPage(typeof(ModsPage));
+        }
+    }
+}
